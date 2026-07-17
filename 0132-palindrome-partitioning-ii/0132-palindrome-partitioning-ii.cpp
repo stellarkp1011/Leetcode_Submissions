cfd6fa45f40f64d248ca00b2b0003deb3fedf7f3
stellarkp1011 <kirtipandz18@gmail.com>
2026-07-17 22:04:20 +0530

    Time: 1496 ms (42.25%), Space: 9.3 MB (87.11%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -26,6 +26,34 @@ public:
     // }
 
     // Memoization
+    // bool isPallindrome(int i, int j, string &str){
+    //     while(i < j){
+    //         if(str[i] != str[j]) return false;
+    //         i++;
+    //         j--;
+    //     }
+    //     return true;
+    // }
+    // int f(int i, int n, string& s, vector<int> &dp){
+    //     if(i == n) return 0;
+
+    //     if(dp[i] != -1) return dp[i];
+    //     int minCost = INT_MAX;
+    //     for(int j = i; j < n; j++){
+    //         if(isPallindrome(i, j, s)){
+    //             int cost = 1 + f(j + 1, n, s, dp);
+    //             minCost = min(minCost, cost);
+    //         }
+    //     }
+    //     return dp[i] = minCost;
+    // }
+    // int minCut(string s) {
+    //     int n = s.size();
+    //     vector<int> dp(n, -1);
+    //     return f(0, n, s, dp) - 1;
+    // }
+
+    // Tabulation 
     bool isPallindrome(int i, int j, string &str){
         while(i < j){
             if(str[i] != str[j]) return false;
@@ -34,22 +62,20 @@ public:
         }
         return true;
     }
-    int f(int i, int n, string& s, vector<int> &dp){
-        if(i == n) return 0;
-
-        if(dp[i] != -1) return dp[i];
-        int minCost = INT_MAX;
-        for(int j = i; j < n; j++){
-            if(isPallindrome(i, j, s)){
-                int cost = 1 + f(j + 1, n, s, dp);
-                minCost = min(minCost, cost);
-            }
-        }
-        return dp[i] = minCost;
-    }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n, -1);
-        return f(0, n, s, dp) - 1;
+        vector<int> dp(n + 1, -1);
+        dp[n] = 0;
+        for(int i = n - 1; i >= 0; i--){
+            int minCost = INT_MAX;
+            for(int j = i; j < n; j++){
+                if(isPallindrome(i, j, s)){
+                    int cost = 1 + dp[j + 1];
+                    minCost = min(minCost, cost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] - 1;
     }
 };
