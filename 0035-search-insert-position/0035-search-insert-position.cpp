61e7600762990e0d2aca7d70a8f62d1332ca9817
stellarkp1011 <kirtipandz18@gmail.com>
2026-07-18 23:53:31 +0530

    Time: 0 ms (100%), Space: 13.5 MB (78.17%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,15 +4,18 @@ public:
         int low = 0;
         int high = nums.size() - 1; 
         int mid;
+        int ans = nums.size();
         while(low <= high){
             mid = low + (high - low) / 2;
 
-            if(nums[mid] == target) return mid;
-
-            if(target < nums[mid]) high = mid - 1;
-
-            if(target > nums[mid]) low = mid + 1;
+            if(nums[mid] >= target){ 
+                ans = mid;
+                high = mid - 1;
+            }
+            else{ 
+                low = mid + 1;
+            }
         }
-        return low;
+        return ans;
     }
 };
