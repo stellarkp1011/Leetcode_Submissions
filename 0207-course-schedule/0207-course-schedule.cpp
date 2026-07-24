c24016b4d47ca98ee05ab021e350b8eae456a5f1
stellarkp1011 <kirtipandz18@gmail.com>
2026-07-24 23:36:08 +0530

    Time: 4 ms (63.53%), Space: 20.2 MB (24.51%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
+
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int topo = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo++;
+
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        return topo == numCourses;
+    }
+};
