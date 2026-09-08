1401e207093744695cb2647774ea0db9d6ddbd3f
KirtiPandey <kirtipandz18@gmail.com>
2026-09-09 00:55:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.51%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int sum = 0;
-        for(int i = 1; i <= n; i++) {
-            if(i >= 1000) {
-                sum += 1;
-            }
+        int count = 0;
+        for(int i = 1000; i <= n; i *= 1000) {
+            count += n - i + 1;
         }
-        return sum;
+        return count;
     }
 };
